3e68ee4bcd1785fd66473b871098e65f95ece6bc
AlvinJangvik <54435532+AlvinJangvik@users.noreply.github.com>
2020-10-13 14:35:13 +0200

    include

--- AlvinWebbserver/templates/sida3-template.php
+++ AlvinWebbserver/templates/sida3-template.php
@@ -30,36 +30,6 @@
 FIGURE;
 				}
 			?>
-          
-            <!--figure><img src="bilder/apple.jpg" alt="Grönt surt">
-                <figcaption>Äpple 50
-                  <p>
-                    <a href="#">Köp</a>
-                  </p>
-                </figcation>
-            </figure>
-            <figure><img src="bilder/orange.jpg" alt="Orange söt">
-                <figcaption>Apelsin 38
-                  <p>
-                    <a href="#">Köp</a>
-                  </p>
-                </figcation>
-            </figure>
-            <figure><img src="bilder/pear.jpg" alt="Gult saftigt">
-                <figcaption>Päron 100
-                  <p>
-                    <a href="#">Köp</a>
-                  </p>
-                </figcation>
-            </figure>
-            <figure><img src="bilder/banana.jpg" alt="Gul böjd">
-                <figcaption>Banan 30
-                  <p>
-                    <a href="#">Köp</a>
-                  </p>
-                </figcation>
-            </figure-->
-            
 			</section>
 				
 				
