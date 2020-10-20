14da90fd14a78a21693168b13620bd977e5c855c
AlvinJangvik <54435532+AlvinJangvik@users.noreply.github.com>
2020-10-20 14:42:06 +0200

    Add files via upload

--- AlvinWebbserver/includes/connect.php
+++ AlvinWebbserver/includes/connect.php
@@ -17,14 +17,66 @@
 		exit;
 	}
 	
-	$dbh->close();
+	//$dbh->close();
 	
 	// var_dump($result);
+	echo "<table>";
+			while($row = $result->fetch_assoc()){
+				echo "<tr>";
+					echo "<td>";
+						echo $row['name'];
+					echo "</td>";
+					echo "<td>";
+						echo $row['price']. " kronor";
+					echo "</td>";
+				echo "</tr>";
+			}
+	echo "</table>";
 	
-	while($row = $result->fetch_assoc()){
-		echo $row['name'];
-		echo "<br>";
-		echo $row['price']. " kronor";
-		echo "<br> <br>";
+	echo "<br><br>";;
+
+	$sql = "SELECT users.user, users.email, customers.username, customers.firstname, customers.lastname FROM users JOIN customers WHERE users.user = customers.username";
+
+	$res = $dbh->prepare($sql);
+	$res->execute();
+	$result = $res->get_result();
+	// var_dump($result);
+
+echo "<table><tr><th>Anv namn</th><th>Förnamn</th><th>Efternamn</th></tr>";
+	while($row = $result->fetch_assoc())
+	{
+		echo "<tr><td>";
+		echo $row['username'];
+		echo "</td><td>";
+		echo $row['firstname'];
+		echo "</td><td>";
+		echo $row['lastname'];
+		echo "</td><td>";
+		echo $row['email'];
+		echo "</td></tr>";
 	}
+echo "</table>";
+
+	$sql = "SELECT customers.customerID, customers.username, customers.firstname, customers.lastname, orders.customerID, orders.productID, orders.orderID, products.productID, products.name FROM order JOIN customers WHERE customers.customerID=orders.custumerID JOIN products WHERE products.productID = orders.productID";
+
+	$res = $dbh->prepare($sql);
+	$res->execute();
+	$result = $res->get_result();
+	
+	echo "<table><tr><th>Anv namn</th><th>Förnamn</th><th>Efternamn</th></tr>";
+	while($row = $result->fetch_assoc())
+	{
+		echo "<tr><td>";
+		echo $row['username'];
+		echo "</td><td>";
+		echo $row['firstname'];
+		echo "</td><td>";
+		echo $row['lastname'];
+		echo "</td><td>";
+		echo $row['email'];
+		echo "</td></tr>";
+	}
+	echo "</table>";
+
 ?>
+
